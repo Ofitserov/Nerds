475f6e3132858466b2f3b97b8b8a93c383069268
Vlad <ofitserov.vladimir@gmail.com>
2016-12-15 14:45:29 +0300

    module

--- js/slider.js
+++ js/slider.js
@@ -1,48 +1,51 @@
-var slides = document.getElementsByName('slider'),
-    slider = document.querySelector('.slider');
+;(function(){
 
-function currentSlide() {
-  for (var i = 0; i < slides.length; i++) {
-    if (slides[i].checked) {
-      var currentSlide = slides[i];
+  var slides = document.getElementsByName('slider'),
+      slider = document.querySelector('.slider');
+
+  function currentSlide() {
+    for (var i = 0; i < slides.length; i++) {
+      if (slides[i].checked) {
+        var currentSlide = slides[i];
+      }
     }
+    return currentSlide;
   }
-  return currentSlide;
-}
 
-function nextSlide() {
-  for (var i = 0; i < slides.length; i++) {
-    if (slides[i].checked) {
-      var nextSlide = i+1 >= slides.length ? slides[0] : slides[i+1];
+  function nextSlide() {
+    for (var i = 0; i < slides.length; i++) {
+      if (slides[i].checked) {
+        var nextSlide = i+1 >= slides.length ? slides[0] : slides[i+1];
+      }
     }
+    return nextSlide;
   }
-  return nextSlide;
-}
 
-function rotate(currentSlide, nextSlide) {
-  currentSlide.removeAttribute('checked');
-  nextSlide.checked = true;
-}
+  function rotate(currentSlide, nextSlide) {
+    currentSlide.removeAttribute('checked');
+    nextSlide.checked = true;
+  }
 
-function startRotate() {
-  return intervalID = setInterval(function(){
-    rotate(currentSlide(), nextSlide())
-  }, 3000);
-}
-function stopRotate() {
-  clearInterval(intervalID);
-}
+  function startRotate() {
+    return intervalID = setInterval(function(){
+      rotate(currentSlide(), nextSlide())
+    }, 2000);
+  }
+  function stopRotate() {
+    clearInterval(intervalID);
+  }
 
 
-  document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function(){
+      startRotate();
+    });
+
+    slider.addEventListener('mouseover', function(event){
+    event.preventDefault;
+    stopRotate();
+  });
+    slider.addEventListener('mouseout', function(event){
+    event.preventDefault;
     startRotate();
   });
-
-  slider.addEventListener('mouseover', function(event){
-  event.preventDefault;
-  stopRotate();
-});
-  slider.addEventListener('mouseout', function(event){
-  event.preventDefault;
-  startRotate();
-});
+})();
